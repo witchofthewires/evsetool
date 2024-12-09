b3aa61a589d8c8f1daab3b09e2539b1f1c2dfa50
dani <dcm678@gmail.com>
2024-12-09 17:20:24 -0500

    cleaned up further

--- evsetool.py
+++ evsetool.py
@@ -13,22 +13,30 @@ from ocpp.v16 import call
 from ocpp.v16 import ChargePoint as cp
 
 CSMS_URL = "ws://127.0.0.1:8180/steve/websocket/CentralSystemService"
+ID_TAG = "01234567890123456789"
+MODEL = "WW1959"
+VENDOR = "WitchWires"
+CP_NAME = "CP_1"
+CONNECTOR_ID = 1
+METER_START = 1000
+METER_STOP = 2000
 
 logging.basicConfig(level=logging.INFO)
 
-class ChargePoint(cp):
+class OCPPv16ChargePoint(cp):
 
     def __init__(self, *args, **kwargs):
         self.transactions = []
+        self.id_tag = ID_TAG if 'id_tag' not in kwargs else kwargs['id_tag']
         super().__init__(*args, **kwargs)
 
     async def send_boot_notification(self):
-        request = call.BootNotification(charge_point_model="WW1959", charge_point_vendor="WitchWires")
-        response = await self.call(request)
+        req = call.BootNotification(charge_point_model=MODEL, charge_point_vendor=VENDOR)
+        res = await self.call(req)
 
     async def send_heartbeat(self):
-        request = call.Heartbeat()
-        response = await self.call(request)
+        req = call.Heartbeat()
+        res = await self.call(req)
 
     async def beat_heart(self):
         while True: 
@@ -36,36 +44,31 @@ class ChargePoint(cp):
             await self.send_heartbeat()
 
     async def authorize(self, id_tag):
-        request = call.Authorize(id_tag)
-        response = await self.call(request)
+        req = call.Authorize(id_tag)
+        res = await self.call(req)
 
     async def start_transaction(self, connector_id, id_tag, meter_start, timestamp, reservation_id=None):
-        request = call.StartTransaction(connector_id, id_tag, meter_start, timestamp, reservation_id=reservation_id)
-        response = await self.call(request)
-        self.transactions.append(response.transaction_id)
+        req = call.StartTransaction(connector_id, id_tag, meter_start, timestamp, reservation_id=reservation_id)
+        res = await self.call(req)
+        self.transactions.append(res.transaction_id)
 
     async def stop_transaction(self, id_tag, meter_stop, timestamp, transaction_id, reason=None, transaction_data=None):
-        request = call.StopTransaction(meter_stop, timestamp, transaction_id, reason, id_tag, transaction_data)
-        await self.call(request)
+        req = call.StopTransaction(meter_stop, timestamp, transaction_id, reason, id_tag, transaction_data)
+        await self.call(req)
 
-async def run_chargepoint(name = None, id_tag='01234567890123456789'):
-    if name is None: name = 'CP_1'
-    connector_id = 1
-    meter_start = 1000
-    meter_stop = 2000
+async def run_chargepoint(name = None):
+    if name is None: name = CP_NAME
     reservation_id = None
-    timestamp = rightnow()
 
     async with websockets.connect('%s/%s' % (CSMS_URL, name),
                                   subprotocols=['ocpp1.6']) as ws:
-        cp = ChargePoint(name, ws)
-        await asyncio.gather(cp.start(), cp.send_boot_notification(), cp.authorize(id_tag),
-                             cp.start_transaction(connector_id, id_tag, meter_start, timestamp, reservation_id),
-                             cp.stop_transaction(id_tag, meter_stop, transaction_id=1, timestamp=rightnow(), reason=None, transaction_data=None))
+        cp = OCPPv16ChargePoint(name, ws)
+        await asyncio.gather(cp.start(), cp.send_boot_notification(), cp.authorize(cp.id_tag),
+                             cp.start_transaction(CONNECTOR_ID, cp.id_tag, METER_START, timestamp=rightnow(), reservation_id=reservation_id),
+                             cp.stop_transaction(cp.id_tag, METER_STOP, transaction_id=1, timestamp=rightnow(), reason=None, transaction_data=None))
 
 def rightnow():
     return datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
 if __name__ == '__main__':
-    name = sys.argv[1] if len(sys.argv) >= 2 else None
-    asyncio.run(run_chargepoint(name))
+    asyncio.run(run_chargepoint())
