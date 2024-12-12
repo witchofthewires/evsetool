80ad03309f4080cb1b8c4b1f12440e7d3317e9b4
witchofthewires <ghost@ratslayer.lan>
2024-12-12 14:26:11 -0500

    implemented single CLI interface

--- src/evsetool.py
+++ src/evsetool.py
@@ -116,8 +116,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='EVSE Red Team Tool')
     parser.add_argument('-v', '--verbose', action='store_true',
                     help='Show full OCPP traffic output')
-    parser.add_argument('--csms', type=str,
-                    help='URL of CSMS to query')
+    parser.add_argument('--sniff', action='store_true',
+                    help='Listen passively for OCPP1.6 traffic over TCP 8180')
+    parser.add_argument('--csms', action='store_true',
+                    help='Query CSMS as simulated EVSE')
+    parser.add_argument('--url', type=str,
+                    help='Address of CSMS to query')
     parser.add_argument('-i', '--id', type=str,
                     help='20-character string uniquely identifying the EVSE')
     parser.add_argument('--vendor', type=str, default='WitchWires',
@@ -128,9 +132,18 @@ if __name__ == '__main__':
                     help='API endpoint for the EVSE')
     
     args = parser.parse_args()
-    url = CSMS_URL if args.csms is None else args.csms
+    url = CSMS_URL if args.url is None else args.url
     id_tag = ID_TAG if args.id is None else args.id
     log_level = logging.INFO if args.verbose else logging.ERROR
     logging.basicConfig(level=log_level)
     args = parser.parse_args()
-    asyncio.run(simflow_diagnostics(url, id_tag, args.name))
+
+    if args.sniff:
+        print("Starting sniffer...")
+        sniffer.main()
+    elif args.csms:
+        print("Querying CSMS...")
+        asyncio.run(simflow_diagnostics(url, id_tag, args.name))
+    else:
+        print("ERROR: Please select one of the following: [sniff|csms]")
+        print("use --help for more information")
