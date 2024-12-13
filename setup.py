9854c752c101bb66f0f9328a7b74bd42b4540f7d
witchofthewires <ghost@ratslayer.lan>
2024-12-13 16:50:02 -0500

    fixed module import issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 name='evsetool',
-version='0.1.0',
+version='0.1.1',
 author='Danielle McGuire',
 author_email='dcm678@gmail.com',
 description='EVSE Red Team Swiss Army CLI',
