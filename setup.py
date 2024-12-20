f7cef22421905e30c39ff2fe8ee2b9d10577444a
dani <dcm678@gmail.com>
2024-12-20 13:09:01 -0500

    moving to new branch

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+name='evsetool',
+version='0.1.3',
+author='Danielle McGuire',
+author_email='dcm678@gmail.com',
+description='EVSE Red Team Swiss Army CLI',
+packages=find_packages(where='src\evsetool'),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
