fb72f683a9981281f10acd43d8db596946f63da2
witchofthewires <ghost@ratslayer.lan>
2025-01-06 13:07:38 -0500

    committed to wrong branch

--- src/evsetool/__main__.py
+++ src/evsetool/__main__.py
@@ -2,7 +2,8 @@ import argparse
 import logging
 import asyncio
 import yaml
-from scapy.all import rdpcap
+import scapy
+import time
 
 from sniffer import parse, main as sniff
 from evse import simflow_transaction, simflow_diagnostics
@@ -50,7 +51,7 @@ if args.sniff:
     sniff()
 elif args.pcap:
     log("EVSETOOL::Reading pcap <%s>..." % args.pcap)
-    pkt = rdpcap(args.pcap)
+    pkt = scapy.all.rdpcap(args.pcap)
     for p in map(parse, pkt): 
         if p is not None: print(p)         
 elif args.csms:
@@ -62,3 +63,47 @@ elif args.serve:
 else:
     print("ERROR: Please select one of the following: [sniff|pcap|csms]")
     print("use --help for more information")
+    args.file = './default.config.yaml' if not args.file else args.file
+    with open(args.file) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+                        
+    url = cfg['url'] if args.url is None else args.url
+    id_tag = cfg['id_tag'] if args.id is None else args.id
+    log_level = logging.INFO if args.verbose else logging.ERROR
+    logging.basicConfig(level=log_level)
+    args = parser.parse_args()
+
+    if args.sniff:
+        log("EVSETOOL::Starting sniffer...")
+        sniff()
+    elif args.pcap:
+        log("EVSETOOL::Reading pcap <%s>..." % args.pcap)
+        pkt = scapy.all.rdpcap(args.pcap)
+        for p in map(parse, pkt): 
+            if p is not None: print(p)         
+    elif args.csms:
+        log("EVSETOOL::Querying CSMS...")
+        asyncio.run(simflow_transaction(url, id_tag, args.name))
+    elif args.serve:
+        log("EVSETOOL::Serving OCPP1.6 on port %d" % cfg['local_ocpp_port'])
+        asyncio.run(serve_OCPPv16('0.0.0.0', cfg['local_ocpp_port']))
+    elif args.sim:
+        log("EVSETOOL::Running sim_diagnostics")
+        ip_addr = '127.0.0.1'
+        port = cfg['local_ocpp_port']
+        await run_sim(ip_addr, port, id_tag, args.name)
+        #sniff()
+    else:
+        print("ERROR: Please select one of the following: [csms|sim|sniff|pcap|]")
+        print("use --help for more information")
+
+async def run_sim(ip_addr, port, id_tag, name):
+    log('ere')
+    server_url = 'ws://%s:%s' % (ip_addr, port)
+    
+    loop = asyncio.get_running_loop()
+    loop.create_task(serve_OCPPv16(ip_addr, port))
+    loop.create_task(simflow_diagnostics(server_url, id_tag, name))
+
+if __name__ == '__main__':
+    asyncio.run(main())
